89b78f5ad97ec0de310b4515eb1938bf104cd323
unknown <daniel.atdrroque05@gmail.com>
2020-01-21 10:55:29 -0300

    update

---
+++ app/src/main/java/estudo/com/mapalocal/dao/LocalDAO.kt
@@ -0,0 +1,49 @@
+package estudo.com.mapalocal.dao
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import estudo.com.mapalocal.modelo.Categoria
+
+class LocalDAO(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, name, factory, version) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        val sqlLocal = ""
+
+        val sqlCategoria = ""
+
+        db.execSQL(sqlLocal)
+        db.execSQL(sqlCategoria)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        val localSql = ""
+
+        val categoriaSql = ""
+
+        db.execSQL(localSql)
+        db.execSQL(categoriaSql)
+    }
+
+    fun insertCategoria(categoria: Categoria){
+        val insertCategoria = ""
+    }
+
+    fun updateCategoria(){
+
+    }
+
+    fun selectCategoria(descricao : String){
+        val sql = "SELECT descricao FROM sqlCategoria WHERE descricao = "
+    }
+
+    fun selectAllCategorias(){
+        val sqlAllCategoria = "SELECT * FROM sqlCategoria"
+
+    }
+}
